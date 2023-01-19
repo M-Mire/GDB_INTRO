e9580d8bdd45808fcdf5fa6d12f5c903bad8ba19
M-Mire <73060959+M-Mire@users.noreply.github.com>
2023-01-19 09:36:10 +0000

    Create example1.cpp

---
+++ example1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello and welcome to my GDB Intro" << endl;
+	int a = 4;
+	int b = 8;
+
+	a+=1;
+	int product = a * b;
+	cout << product << endl;
+    return 0;
+}
